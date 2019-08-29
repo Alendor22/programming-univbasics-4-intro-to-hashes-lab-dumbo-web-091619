cb459cc354981d0c7944f4b79b90895dc53ca6b5
Adeja Lendor <alendor6@gmail.com>
2019-08-29 06:37:18 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,5 +29,6 @@ def update_counting_hash(hash, key)
   # if the provided key is present, increment its value by 1
   if update_counting_hash[key]
     update_counting_hash[key] += 1 
-  else update_counting_hash
+  else update_counting_hash[key] = 1 
+end
 end
