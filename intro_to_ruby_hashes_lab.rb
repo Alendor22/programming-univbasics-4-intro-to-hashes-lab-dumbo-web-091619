bc50d97b00ce200f30ca2a64ee9b28d3d02be790
Adeja Lendor <alendor6@gmail.com>
2019-08-29 06:09:21 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,7 +15,7 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
-  my_hash_creator = {}
+  my_hash_creator = {:name => "Grace Hopper"}
   if my_hash_creator[:name]
   else my_hash_creator[:name] = "Grace Hopper"
   end 
